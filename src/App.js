18ef70e60e98bee7d49a160718791b15994327e8
Savan Fami <savanfami1239@gmail.com>
2024-04-20 15:17:14 +0530

    add editing todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React,{useState} from 'react';
-
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editIndex, setEditIndex] = useState(-1); // Track the index of the todo item being edited
+  const [editText, setEditText] = useState(''); // Track the text of the todo item being edited
 
-  const[todos,setTodos]=useState([])
-  const [inputValue,setInputValue]=useState('')
-
-  const handleAddTodo=()=>{
-    if(inputValue.trim()!==''){
-      setTodos([...todos,inputValue])
-      setInputValue('')
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
     }
-  }
-  const handleRemoveTodo=(index)=>{
-      const updatedTodos=todos.filter((todo,i)=>i!==index)
-        setTodos(updatedTodos)
+  };
 
+  const handleRemoveTodo = (index) => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this todo?');
+    if (isConfirmed) {
+      const updatedTodos = todos.filter((_, i) => i !== index);
+      setTodos(updatedTodos);
     }
-  
+  };
+
+  const handleEditTodo = (index) => {
+    setEditIndex(index); // Set the index of the todo item being edited
+    setEditText(todos[index]); // Set the text of the todo item being edited
+  };
+
+  const handleSaveEdit = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = editText;
+    setTodos(updatedTodos);
+    setEditIndex(-1); // Reset the edit index after saving
+  };
 
   return (
     <div className="app">
@@ -28,24 +41,42 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        
       </div>
       <div className="input">
-        <input type="text" onChange={(e)=>setInputValue(e.target.value)} value={inputValue} placeholder="🖊️ Add item..." />
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          placeholder="🖊️ Add item..."
+        />
         <i onClick={handleAddTodo} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-  {todos.map((todo, index) => (
-    <div className="todo" key={index}>
-      <div className="left">
-        <p>{todo}</p>
+        {todos.map((todo, index) => (
+          <div className="todo" key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button onClick={() => handleSaveEdit(index)}>Save</button>
+              </>
+            ) : (
+              <>
+                <div className="left">
+                  <p>{todo}</p>
+                </div>
+                <div className="right">
+                  <i onClick={() => handleEditTodo(index)} className="fas fa-edit"></i>
+                  <i onClick={() => handleRemoveTodo(index)} className="fas fa-times"></i>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
       </div>
-      <div className="right">
-        <i onClick={()=>handleRemoveTodo(index)} className="fas fa-times"></i>
-      </div>
-    </div>
-  ))}
-</div>
     </div>
   );
 }
